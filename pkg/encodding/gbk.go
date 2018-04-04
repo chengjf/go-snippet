99d18a6b8b321f4599db286d10b279b5cdd547cd
Jeff Cheng <jeff@JeffdeMacBook-Pro.local>
2018-04-04 14:41:10 +0800

    流转换流，使用gbk解码

--- pkg/encodding/gbk.go
+++ pkg/encodding/gbk.go
@@ -1,9 +1,12 @@
 package main
 
-import "fmt"
-import "os"
-import "bufio"
-import "golang.org/x/text/encoding/simplifiedchinese"
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"io"
+	"os"
+)
 
 func check(e error) {
 	if e != nil {
@@ -11,7 +14,7 @@ func check(e error) {
 	}
 }
 
-func main() {
+func test() {
 	file, err := os.Open("./aa.csv")
 	check(err)
 	// bufferedReader := bufio.NewReader(file)
@@ -25,4 +28,38 @@ func main() {
 	s, err := bufferedReader2.ReadString('\n')
 	check(err)
 	fmt.Println(s)
+}
+
+// 将一个io.Reader使用GBK解码，转为bufio.Reader
+func getGBKBufReader(reader io.Reader) (result *bufio.Reader) {
+	gbk := simplifiedchinese.GBK
+	gbkReader := gbk.NewDecoder().Reader(reader)
+	result = bufio.NewReader(gbkReader)
+	return
+}
+
+// 将一个io.Reader使用GBK解码，转为io.Reader
+func getGBKReader(reader io.Reader) (result io.Reader) {
+	gbk := simplifiedchinese.GBK
+	result = gbk.NewDecoder().Reader(reader)
+	return
+}
+
+func main() {
+	file, err := os.Open("./aa.csv")
+	check(err)
+	bufferedReader := bufio.NewReader(file)
+	gbkReader := getGBKBufReader(bufferedReader)
+	s, err := gbkReader.ReadString('\n')
+	check(err)
+	fmt.Println(s)
+
+	file, err = os.Open("./aa.csv")
+	check(err)
+	l := getGBKReader(file)
+	buf := make([]byte, 1024)
+	o, p:= l.Read(buf)
+	fmt.Printf("%v", buf)
+	fmt.Printf("%v %v %v \n", o, p, string(buf))
+
 }
